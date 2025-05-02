16f24260a251d0438c4580967e48c9dfff07190e
mpayalal <mpayalal@eafit.edu.co>
2025-05-02 01:29:40 -0500

    Cambio variable entorno

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from routes import upload
+import os
 
 app = FastAPI()
 
 app.include_router(upload.router)
 
+@app.get("/check-credentials")
+async def check_credentials():
+    creds_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+    return {"GOOGLE_APPLICATION_CREDENTIALS": creds_path}
