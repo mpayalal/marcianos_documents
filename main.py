d9c1002ec5b72e06da2b13f11a19d2acb2b9d3e7
mpayalal <mpayalal@eafit.edu.co>
2025-05-02 01:37:54 -0500

    Agregar decode base64 secret

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from routes import upload
 import os
+import base64
+from google.oauth2 import service_account
 
 app = FastAPI()
 
@@ -9,4 +11,11 @@ app.include_router(upload.router)
 @app.get("/check-credentials")
 async def check_credentials():
     creds_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-    return {"GOOGLE_APPLICATION_CREDENTIALS": creds_path}
+
+    # Decodificar las credenciales desde base64
+    decoded_credentials = base64.b64decode(creds_path)
+
+    # Cargar las credenciales en Google Cloud
+    credentials = service_account.Credentials.from_service_account_info(decoded_credentials)
+
+    return {"GOOGLE_APPLICATION_CREDENTIALS": credentials}
--- routes/upload.py
+++ routes/upload.py
@@ -4,6 +4,13 @@ from gcstorage_class import GCStorage
 
 router = APIRouter()
 
+
+import os
+import base64
+from google.oauth2 import service_account
+
+
+
 @router.post("/uploadDocument")
 async def upload_file_to_user_bucket(
     file: UploadFile = File(...),
@@ -11,8 +18,16 @@ async def upload_file_to_user_bucket(
 ):
     try:
         bucket_name = username.lower()
+        
+        # Obtener las credenciales desde la variable de entorno
+        encoded_credentials = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
 
-        gcs = GCStorage(storage.Client())
+        # Decodificar las credenciales desde base64
+        decoded_credentials = base64.b64decode(encoded_credentials)
+
+        # Cargar las credenciales en Google Cloud
+        credentials = service_account.Credentials.from_service_account_info(decoded_credentials)
+        gcs = GCStorage(storage.Client(credentials=credentials))
         
         if not bucket_name in gcs.list_buckets():
             bucket_gcs = gcs.create_bucket(bucket_name, 'STANDARD')
