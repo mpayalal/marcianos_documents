1686e68bc7e001fed65a614d080931bc43eb849d
mpayalal <mpayalal@eafit.edu.co>
2025-05-02 21:30:12 -0500

    Cambio de secret - prueba

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI, HTTPException
 from routes import upload
 from google.cloud import storage
 from google.auth.exceptions import DefaultCredentialsError
+from google.oauth2 import service_account
+import os
+import json
 
 app = FastAPI()
 
@@ -10,7 +13,19 @@ app.include_router(upload.router)
 @app.get("/verify-gcs")
 def verify_gcs_credentials():
     try:
-        client = storage.Client()
+        #prueba
+        # Leer JSON desde variable de entorno
+        json_str = os.getenv("GCP_CREDENTIALS_JSON")
+        if not json_str:
+            raise RuntimeError("GCP_CREDENTIALS_JSON not set")
+
+        # Convertir a dict y crear credenciales
+        info = json.loads(json_str)
+        credentials = service_account.Credentials.from_service_account_info(info)
+
+        #fin prueba
+
+        client = storage.Client(credentials=credentials)
         buckets = list(client.list_buckets())
         bucket_names = [bucket.name for bucket in buckets]
         return {"status": "success", "buckets": bucket_names}
--- routes/upload.py
+++ routes/upload.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, UploadFile, File, HTTPException, Form
 from google.cloud import storage
 from gcstorage_class import GCStorage
+from google.oauth2 import service_account
+import os
+import json
 
 router = APIRouter()
 
@@ -12,7 +15,23 @@ async def upload_file_to_user_bucket(
     try:
         # bucket_name = username.lower()
         bucket_name = "mpayalal"
-        gcs = GCStorage(storage.Client())
+
+        #prueba
+        # Leer JSON desde variable de entorno
+        json_str = os.getenv("GCP_CREDENTIALS_JSON")
+        if not json_str:
+            raise RuntimeError("GCP_CREDENTIALS_JSON not set")
+
+        # Convertir a dict y crear credenciales
+        info = json.loads(json_str)
+        credentials = service_account.Credentials.from_service_account_info(info)
+
+        # Usar las credenciales explícitas
+        gcs = GCStorage(storage.Client(credentials=credentials))
+
+        #fin prueba
+        
+        # gcs = GCStorage(storage.Client())
         
         if not bucket_name in gcs.list_buckets():
             bucket_gcs = gcs.create_bucket(bucket_name, 'STANDARD')
