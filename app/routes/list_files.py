daa7c9aa9351eecdc9a8c67df3435db734cb0aee
Julián David Valencia <julianv302@gmail.com>
2025-05-07 22:37:13 -0500

    fix: add endpoint to list files and generate url with id

--- app/routes/list_files.py
+++ app/routes/list_files.py
@@ -32,22 +32,34 @@ async def list_documents_from_user_folder(
         documentsDb = File.get_all_files_by_user_id(session, user.id)
         if not documentsDb:
             return {"message": f"No se encontraron documentos para el usuario {user.documentNumber}"}
+        
         return documentsDb
         
-        for blob in gcs.list_blobs(bucket_name, f"{folder_name}/"):
-            # Generate signed URL for viewing the file
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.get("/v1/documents/folder/{client_id}")
+async def list_documents_user(client_id: str):
+    try:
+        load_dotenv()
+        creds_path = os.getenv("GCP_SA_KEY")
+        gcs = GCStorage(storage.Client.from_service_account_json(creds_path))
+
+        bucket_name = os.getenv("GCP_BUCKET_NAME")
+        documents = {}    
+
+        for blob in gcs.list_blobs(bucket_name, f"{client_id}/"):
+            # Generara URL prefirmada para ver el archivo
             url = blob.generate_signed_url(
                 version="v4",
                 expiration=datetime.timedelta(minutes=15),
                 method="GET",
             )
-
-            # Use filename as key
+            # Obtener nombre del archivo y ponerlo como llave
             documents[os.path.basename(blob.name)] = {
                 "url": url,
-                "firmado": blob.metadata.get("firmado", "false")
+                "firmado": blob.metadata.get("firmado")
             }
-
         return documents
 
     except Exception as e:
